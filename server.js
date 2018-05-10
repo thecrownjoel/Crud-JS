f6b867881ca361a8d2b88902afdabbe8ca2880a0
Unknown <joel@45press.com>
2018-05-10 17:17:58 -0400

    Complete App

--- server.js
+++ server.js
@@ -22,16 +22,45 @@ MongoClient.connect('mongodb://yoda:jedimaster@ds117540.mlab.com:17540/star-wars
 })
 
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+app.use(express.static('public'))
 
+app.get('/', (req, res) => {
+  db.collection('quotes').find().toArray((err, result) => {
+    if (err) return console.log(err)
+    res.render('index.ejs', {quotes: result})
+  })
+})
 
 app.post('/quotes', (req, res) => {
   db.collection('quotes').save(req.body, (err, result) => {
     if (err) return console.log(err)
-
     console.log('saved to database')
     res.redirect('/')
+  })
+})
+
+app.put('/quotes', (req, res) => {
+  db.collection('quotes')
+  .findOneAndUpdate({name: 'Yoda'}, {
+    $set: {
+      name: req.body.name,
+      quote: req.body.quote
+    }
+  }, {
+    sort: {_id: -1},
+    upsert: true
+  }, (err, result) => {
+    if (err) return res.send(err)
+    res.send(result)
+  })
+})
+
+app.delete('/quotes', (req, res) => {
+  db.collection('quotes').findOneAndDelete({name: req.body.name}, (err, result) => {
+    if (err) return res.send(500, err)
+    res.send('A darth vadar quote got deleted')
   })
 })
